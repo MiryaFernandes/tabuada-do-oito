9d7e7d7a0b733e81aa6f9245c0b684048881fe18
MIRYÃ ELIZABETH FERNANDES BORGES <21276382@CFP127ALUNO.LOCAL>
2021-09-22 14:14:43 -0300

    tabuada do 8 com for

---
+++ src/testeFor.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public abstract class testeFor {
+
+	public static void main(String[] args) {
+		
+		Scanner leitor = new Scanner (System.in);
+		String resposta = "s";
+	
+		
+		for (int x = 0;resposta.equalsIgnoreCase("s");x++) {
+			
+
+			System.out.println(); 
+				
+			
+		
+		}
+
+	}
+
+}
